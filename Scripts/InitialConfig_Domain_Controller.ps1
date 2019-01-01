131efd42f6db57b3559cbf7a5c3c915923dfc9ed
rootsecdev <rootsecdev@gmail.com>
2019-01-01 07:46:16 -0600

    Add files via upload

---
+++ Scripts/InitialConfig_Domain_Controller.ps1
@@ -0,0 +1,4 @@
+New-NetIPaddress -InterfaceIndex 2 -IPAddress 172.16.2.5 -PrefixLength 24 -DefaultGateway 172.16.2.1
+Set-DNSClientServerAddress –InterfaceIndex 2 -ServerAddresses 172.16.2.5,172.16.2.1
+Set-DnsClient -InterfaceIndex 2 -ConnectionSpecificSuffix "seclab.local"
+Rename-Computer -NewName "DC1" -Restart
