f976d9b01e79daa58c45139fa4dfdb29e69a596e
rootsecdev <rootsecdev@gmail.com>
2019-01-01 07:16:05 -0600

    Add files via upload

---
+++ Scripts/Static IP.ps1
@@ -0,0 +1,3 @@
+New-NetIPaddress -InterfaceIndex 2 -IPAddress 172.16.2.5 -PrefixLength 24 -DefaultGateway 172.16.2.1
+Set-DNSClientServerAddress –InterfaceIndex 2 -ServerAddresses 172.16.2.5,1.1.1.1
+Set-DnsClient -InterfaceIndex 2 -ConnectionSpecificSuffix "seclab.local"
